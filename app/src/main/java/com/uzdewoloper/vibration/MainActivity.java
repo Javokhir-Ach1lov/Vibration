cfd8df2db5ec0ea3b3df55924425a18933cf9891
Javohir_Achilov <achilovjavokhir@gmail.com>
2023-02-23 18:14:57 -0700

    Vibration is generated by this code

---
+++ app/src/main/java/com/uzdewoloper/vibration/MainActivity.java
@@ -0,0 +1,41 @@
+package com.uzdewoloper.vibration;
+import static com.uzdewoloper.vibration.R.id.vibrate_btn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    Button vibrate_btn = findViewById(R.id.vibrate_btn);
+
+        Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+
+        final long [] pattern = {2000, 1000};
+
+        vibrate_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (vibrate_btn.getText().toString().equals("Start")){
+                    int repeat = 0;
+                    vibrator.vibrate(pattern, repeat);
+                    Toast.makeText(MainActivity.this, "Started", Toast.LENGTH_SHORT).show();
+                    vibrate_btn.setText("Start");
+                }else{
+                    vibrate_btn.setText("Stop");
+                    Toast.makeText(MainActivity.this, "Canseled", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
